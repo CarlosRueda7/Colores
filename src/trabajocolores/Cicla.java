41949582e0203d6df59a5a02e710208b3cda0c75
Aula L 5-2 <l52@academico.unab.ad>
2022-10-20 11:05:13 -0500

    Taller colores

---
+++ src/trabajocolores/Cicla.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajocolores;
+
+public class Cicla {
+    private String colorCasco;
+    private String colorCicla;
+    private String deporte;
+
+    /**
+     * @return the colorCasco
+     */
+    public String getColorCasco() {
+        return colorCasco;
+    }
+
+    /**
+     * @param colorCasco the colorCasco to set
+     */
+    public void setColorCasco(String colorCasco) {
+        this.colorCasco = colorCasco;
+    }
+
+    /**
+     * @return the colorCicla
+     */
+    public String getColorCicla() {
+        return colorCicla;
+    }
+
+    /**
+     * @param colorCicla the colorCicla to set
+     */
+    public void setColorCicla(String colorCicla) {
+        this.colorCicla = colorCicla;
+    }
+
+    /**
+     * @return the deporte
+     */
+    public String getDeporte() {
+        return deporte;
+    }
+
+    /**
+     * @param deporte the deporte to set
+     */
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+            
+    
+    
+    
+}
